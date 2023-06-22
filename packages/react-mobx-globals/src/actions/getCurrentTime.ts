1bad41eea902cc31c1a8fbd6958773b0576af694
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2023-06-22 14:50:00 +0200

    v3.0.12

--- packages/react-mobx-globals/src/actions/getCurrentTime.ts
+++
@@ -1,8 +0,0 @@
-const BIGINT_ROUNDER = 1000000;
-const TIMING_PRECISION = 3;
-
-export const getCurrentTime = () => {
-  return typeof window !== undefined
-    ? performance.now()
-    : Number((Number(process.hrtime.bigint()) / BIGINT_ROUNDER).toFixed(TIMING_PRECISION));
-};
