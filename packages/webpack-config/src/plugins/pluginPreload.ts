6faf73f84088e7d0cb8e48d1c9cca56b6ea6dbc3
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2023-10-19 18:34:21 +0200

    [master] allow preload woff fonts

--- packages/webpack-config/src/plugins/pluginPreload.ts
+++ packages/webpack-config/src/plugins/pluginPreload.ts
@@ -10,11 +10,12 @@ import { TypePlugin } from '../types';
 export const pluginPreload: TypePlugin = new PreloadWebpackPlugin({
   rel: 'preload',
   as(filePath) {
+    if (filePath.endsWith('.woff')) return 'font';
     if (filePath.endsWith('.woff2')) return 'font';
     if (filePath.endsWith('.png') || filePath.endsWith('.jpg')) return 'image';
 
     return 'script';
   },
   include: 'allAssets',
-  fileWhitelist: [/\.woff2$/],
+  fileWhitelist: [/\.woff2?$/],
 });
