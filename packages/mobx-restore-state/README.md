679e08947336f901bdba8aa7e1ca714842314acd
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2024-04-11 19:54:10 +0200

    [master] deprecate restore-state

--- packages/mobx-restore-state/README.md
+++ packages/mobx-restore-state/README.md
@@ -1,1 +1,41 @@
-Work in progress
+## Library for merging observables (DEPRECATED)
+
+![coverage](https://github.com/dkazakov8/dk-framework/blob/master/packages/mobx-restore-state/cover.svg)
+[![npm](https://img.shields.io/npm/v/dk-mobx-restore-state)](https://www.npmjs.com/package/dk-mobx-restore-state)
+[![license](https://img.shields.io/npm/l/dk-mobx-restore-state)](https://github.com/dkazakov8/dk-framework/blob/master/packages/mobx-restore-state/LICENSE)
+
+> [!WARNING]  
+> It's fine if you use this library from NPM package with a **static versioning** in case you
+> want it for some pet-project or to test it's capabilities.
+>
+> But for production use it's **strongly recommended** to create a fork, because I do not write
+> Changelogs and may break / add some functionality without notice.
+
+The purpose of this library is to safely restore state during SSR.
+
+MobX 4 had a bug where newly added object were not observable, like
+
+```typescript
+const target = observable({ str: '123' });
+const source = { str: '321', obj: {} };
+
+const result = Object.assign(target, source);
+
+expect(result).to.deep.eq(source);
+expect(isObservable(result)).to.deep.eq(true);
+expect(isObservable(result.obj)).to.deep.eq(false); // BUG
+```
+
+Nowadays MobX 5 / 6 versions do not have this bug, we can safely use `Object.assign`. So this library
+is **DEPRECATED** and may be used in legacy-projects only. So with MobX 4 use this:
+
+```typescript
+const target = observable({ str: '123' });
+const source = { str: '321', obj: {} };
+
+const result = restoreState(target, source);
+
+expect(result).to.deep.eq(source);
+expect(isObservable(result)).to.deep.eq(true);
+expect(isObservable(result.obj)).to.deep.eq(true); // NO BUGS
+```
--- packages/mobx-restore-state/test/restoreState.test.ts
+++ packages/mobx-restore-state/test/restoreState.test.ts
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
-import { observable } from 'mobx';
+import { isObservable, observable } from 'mobx';
 
 import { restoreState } from '../src/restoreState';
 
 describe('restoreState', function test() {
-  it('merges correctly', () => {
+  it('merges with restoreState', () => {
     const target = observable({
       arr: [],
       obj: {},
@@ -24,11 +24,77 @@ describe('restoreState', function test() {
       },
       obj4: {},
       str: '123',
+      str2: '123',
+      num: 123,
+      num2: 123,
+      nonExistent1: null,
+      nonExistent2: undefined,
+      nonExistent3: null,
+    };
+
+    const result = restoreState({ logs: true, target, source });
+
+    expect(result).to.deep.eq(source);
+
+    expect(isObservable(result)).to.deep.eq(true);
+    expect(isObservable(result.arr)).to.deep.eq(true);
+    expect(isObservable(result.arr[0])).to.deep.eq(true);
+    expect(isObservable(result.arr[0].obj3)).to.deep.eq(true);
+    expect(isObservable(result.obj)).to.deep.eq(true);
+    expect(isObservable(result.obj4)).to.deep.eq(true);
+    expect(isObservable(result.obj.obj2)).to.deep.eq(true);
+  });
+
+  it('merges incorrectly with Object.assign', () => {
+    const target = observable({
+      arr: [],
+      obj: {},
+      str: '123',
       num: 123,
       nonExistent1: null,
       nonExistent2: undefined,
+    });
+
+    const source = {
+      arr: [{ obj3: { param: '123' } }],
+      obj: {
+        obj2: {
+          obj3: { param: '123' },
+        },
+        str: '123',
+      },
+      obj4: {},
+      str: '123',
+      str2: '123',
+      num: 123,
+      num2: 123,
+      nonExistent1: null,
+      nonExistent2: undefined,
+      nonExistent3: null,
     };
 
-    expect(restoreState({ logs: true, target, source })).to.deep.eq(source);
+    const result2 = Object.assign(target, source) as any;
+
+    expect(result2).to.deep.eq(source);
+
+    expect(isObservable(result2)).to.deep.eq(true);
+    expect(isObservable(result2.arr)).to.deep.eq(true);
+    expect(isObservable(result2.arr[0])).to.deep.eq(true);
+    expect(isObservable(result2.arr[0].obj3)).to.deep.eq(true);
+    expect(isObservable(result2.obj)).to.deep.eq(true);
+    expect(isObservable(result2.obj4)).to.deep.eq(false); // BUG
+    expect(isObservable(result2.obj.obj2)).to.deep.eq(true);
+    expect(isObservable(result2.obj.obj2.obj3)).to.deep.eq(true);
+  });
+
+  it('merges incorrectly with Object.assign (simplified)', () => {
+    const target = observable({ str: '123' });
+    const source = { str: '321', obj: {} };
+
+    const result = Object.assign(target, source);
+
+    expect(result).to.deep.eq(source);
+    expect(isObservable(result)).to.deep.eq(true);
+    expect(isObservable(result.obj)).to.deep.eq(false); // BUG
   });
 });
