801314f2a9aca5e876f8efce0b7885659af64e56
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2024-05-28 22:46:38 +0200

    [master] update router

--- packages/react-mobx-router/src/index.ts
+++ packages/react-mobx-router/src/index.ts
@@ -3,10 +3,12 @@ export { getInitialRoute } from './utils/getInitialRoute';
 export { createRouterConfig } from './createRouterConfig';
 export { redirectToGenerator } from './redirectToGenerator';
 export { findRouteByPathname } from './utils/findRouteByPathname';
+export { replaceDynamicValues } from './utils/replaceDynamicValues';
 export { loadComponentToConfig } from './utils/loadComponentToConfig';
 
 export { Router } from './Router';
 
+export type { TypeRoute } from './types/TypeRoute';
 export type { TypeCurrentRoute } from './types/TypeCurrentRoute';
 export type { TypeRedirectToParams } from './types/TypeRedirectToParams';
 export type { InterfaceRouterStore } from './types/InterfaceRouterStore';
