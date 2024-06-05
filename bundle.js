c4fb60bded3f30785395cc7b7af96a9f8e17e09b
Dmitry Kazakov <dmitry.kazakov.developer@gmail.com>
2024-06-05 19:48:32 +0200

    [master] fix cjs

--- bundle.js
+++ bundle.js
@@ -53,7 +53,11 @@ function saveSizeLog(prevSize, size) {
 }
 
 function afterBuild(result) {
-  const size = bytesForHuman(result.metafile.outputs[pkg.main].bytes);
+  const size = bytesForHuman(
+    result.metafile.outputs[
+      pkg.name === 'dk-webpack-parallel-simple' ? pkg.main : pkg.exports.import.replace('./', '')
+    ].bytes
+  );
   const prevSize = getPrevSize();
 
   setTimeout(() => saveSizeLog(prevSize, size), 1);
@@ -78,8 +82,11 @@ function afterBuild(result) {
 const buildConfig = {
   entryPoints: [path.resolve(process.cwd(), 'src')],
   bundle: true,
-  format: isNode ? 'cjs' : 'esm',
-  outfile: path.resolve(process.cwd(), path.resolve(process.cwd(), pkg.main)),
+  format: 'esm',
+  outfile: path.resolve(
+    process.cwd(),
+    path.resolve(process.cwd(), pkg.exports?.import || pkg.main)
+  ),
   write: true,
   minify: false,
   metafile: false,
@@ -93,21 +100,27 @@ const buildConfig = {
 
 return Promise.resolve()
   .then(() => {
-    if (pkg.name === 'dk-webpack-parallel-simple') return Promise.resolve();
+    if (!pkg.exports?.import) return Promise.resolve();
 
-    return esbuild.build(buildConfig);
+    return Promise.all([
+      esbuild.build(buildConfig),
+      esbuild.build({
+        ...buildConfig,
+        format: 'cjs',
+        outfile: path.resolve(process.cwd(), path.resolve(process.cwd(), pkg.exports.require)),
+      }),
+    ]);
   })
   .then(() =>
-    esbuild.build(
-      Object.assign({}, buildConfig, {
-        entryPoints: [path.resolve(process.cwd(), pkg.main)],
-        format: 'cjs',
-        write: false,
-        minify: true,
-        metafile: true,
-        sourcemap: false,
-        packages: isNode ? 'external' : undefined,
-      })
-    )
+    esbuild.build({
+      ...buildConfig,
+      entryPoints: [path.resolve(process.cwd(), pkg.exports?.import || pkg.main)],
+      format: 'cjs',
+      write: false,
+      minify: true,
+      metafile: true,
+      sourcemap: false,
+      packages: isNode ? 'external' : undefined,
+    })
   )
   .then(afterBuild);
